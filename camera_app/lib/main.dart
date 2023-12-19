e4ecb55a7032fd990cb38d2f678567532181689b
gedeastu <gede.astu@gmail.com>
2023-12-19 17:58:17 +0700

    up : pub depeden get

--- camera_app/lib/main.dart
+++ camera_app/lib/main.dart
@@ -15,6 +15,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
--- camera_app/lib/pages/login.dart
+++ camera_app/lib/pages/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -11,6 +11,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       body: ,
     );
   }
