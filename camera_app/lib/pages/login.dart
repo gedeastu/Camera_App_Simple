c77a9f3604d4a443edecd800099117d7b9cc352b
gedeastu <gede.astu@gmail.com>
2023-12-19 18:45:28 +0700

    up

--- camera_app/lib/pages/login.dart
+++ camera_app/lib/pages/login.dart
@@ -11,8 +11,44 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: ,
+      backgroundColor: Color.fromARGB(255, 220, 219, 219),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50.0,),
+              //Logo
+              const SizedBox(
+                child: Icon(Icons.lock,size: 100,),
+              ),
+              const SizedBox(height: 50,),
+              //Welcome back you've been missed!
+              const Text(
+                "Welcome back you've been missed!",
+                style: TextStyle(fontSize: 17.0,color: Color.fromARGB(255, 117, 117, 117)),
+              ),
+              Container(
+                width: 400,
+                height: 400,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0)
+                ),
+                child: TextField(
+                onTapOutside: (event) {
+                  
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
