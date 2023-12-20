068f26d565afd4c4b100437f80cbe7af0deab523
gedeastu <gede.astu@gmail.com>
2023-12-20 15:38:11 +0700

    up: make button to navigate login page

--- camera_app/lib/pages/register.dart
+++ camera_app/lib/pages/register.dart
@@ -11,16 +11,30 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Row(
-          children: [],
-        ),
-      ),
       backgroundColor: Colors.grey.shade300,
       body: SafeArea(
-        child: Container(
-          child: 
-        ),
+        child: Center(
+          child: Column(children: [
+
+            //Back to login
+            Container(
+              margin: EdgeInsets.only(left: 20.0,top: 25.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Row(
+                  children: [
+                    Icon(Icons.arrow_back_ios),
+                    Text("Back to Login")
+                  ],
+                ),
+              ),
+            )
+
+            
+          ]),
+        )
       ),
     );
   }
